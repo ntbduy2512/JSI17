81ad4bbdba99cbe8eace2a80a1611b4f2ff19e55
ntbduy2512 <n.t.b.duy07@gmail.com>
2023-03-19 21:29:11 +0700

    buoi2

--- Buoi2/buoi2.js
+++ Buoi2/buoi2.js
@@ -2,11 +2,11 @@
 arr1 = [1,2,3,5]
 const arr2 = [...arr1, arr1]
 console.log(arr2)
-
+// Cau 2
 arrA = ["hello", " Xin chào"]
 arrB = ["Bonjour", "Olá"]
 const waystosayhello = [...arrA, ...arrB]
-
+// Cau 3
 arrX = [0,1,2,3,4,5,6,7,8]
 
 const arrSquare = arrX 
@@ -30,3 +30,4 @@ for(i=0; i<arrOdds.length; i++){
 // })
 
 // console.log(arrOdds);
+// https://docs.google.com/document/d/1fASkBnSu4QrvgfnROstlksPFqkS-X9S91cGz0MQQuF8/edit#
