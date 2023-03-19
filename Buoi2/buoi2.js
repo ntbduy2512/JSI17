7f93afe2bc621be242a6455739e32f6ab1d2836d
ntbduy2512 <n.t.b.duy07@gmail.com>
2023-03-19 21:27:18 +0700

    buoi2

---
+++ Buoi2/buoi2.js
@@ -0,0 +1,32 @@
+// : Cho array sau arr1 = [1,2,3,5]. Hãy duplicate array cho bên trên bằng ít nhất 2 cách; trong đó có 1 cách dùng Spread Operator.
+arr1 = [1,2,3,5]
+const arr2 = [...arr1, arr1]
+console.log(arr2)
+
+arrA = ["hello", " Xin chào"]
+arrB = ["Bonjour", "Olá"]
+const waystosayhello = [...arrA, ...arrB]
+
+arrX = [0,1,2,3,4,5,6,7,8]
+
+const arrSquare = arrX 
+for(i=0; i<arrSquare.length; i++){
+    console.log(arrSquare[i]**2)
+}
+
+const arrOdds = arrX 
+for(i=0; i<arrOdds.length; i++){
+    if( arrOdds[i]%2 !=0){
+        console.log(arrOdds[i])
+    }
+}
+// const arrSquare = arrX.map((num) => {
+//     return num*num;
+// })
+// console.log(arrSquare);
+
+// const arrOdds = arrX.filter((num) => {
+//     return (num %2 == 1);
+// })
+
+// console.log(arrOdds);
